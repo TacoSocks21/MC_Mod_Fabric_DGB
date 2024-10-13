a092d262d3c564ae2151c88726a86f91320027be
TacoSocks21 <tacosocks.gaming@gmail.com>
2024-10-13 14:56:26 -0600

    Added initial setup for mod

---
+++ src/main/java/net/tacosocks/gaming_blocks/DecorativeGamingBlocksClient.java
@@ -0,0 +1,10 @@
+package net.tacosocks.gaming_blocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DecorativeGamingBlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
